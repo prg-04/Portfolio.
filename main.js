8562829885c0112e0884ae0f846d5543219057fb
prg-04 <karanjaevans95@gmail.com>
2023-01-18 19:04:44 +0300

    Modify form input

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 const toggle = document.querySelector('.toggleMenu');
 const navlink = document.querySelector('.navlinks');
 let menuOpen = false;
@@ -12,4 +13,21 @@ toggle.addEventListener('click', () => {
   }
 
   navlink.classList.toggle('active');
+});
+
+const hide = document.getElementById('firstN');
+const hide2 = document.getElementById('lastN');
+const hide3 = document.getElementById('fullname');
+
+window.addEventListener('resize', () => {
+  if (screen.width < 550) {
+    hide.setAttribute('disabled', '');
+    hide2.setAttribute('disabled', '');
+  } if (screen.width > 550) {
+    hide3.setAttribute('disabled', '');
+  } else {
+    hide3.removeAttribute('disabled');
+    hide.removeAttribute('disabled');
+    hide2.removeAttribute('disabled');
+  }
 });
